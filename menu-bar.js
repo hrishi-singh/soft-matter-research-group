199d51235999a152d95311ce7d776fff646d07d4
Hrishi Singh <singh.123@iitj.ac.in>
2022-06-02 11:50:17 +0530

    git commit vers2.0.0

---
+++ menu-bar.js
@@ -0,0 +1,17 @@
+var x=document.getElementById("menu");
+var y=document.getElementById("menuIcon");
+function showfunc(){
+        x.style.display="flex";
+}
+    
+    
+function togglefunc(){
+    if(x.style.display=="flex"){
+        x.style.display="none";
+    }
+    else{
+        x.style.display="flex";
+    }
+    
+    
+}
